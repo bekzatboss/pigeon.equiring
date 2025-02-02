3855eeb751f5f6c9229f7a0ce49370d451f360eb
bekzatboss <165669267+bekzatboss@users.noreply.github.com>
2025-02-02 13:24:16 +0500

    Update AppDelegate.swift
    FirebaseApp.configure()

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
